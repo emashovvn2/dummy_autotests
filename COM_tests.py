6e1b8cd46b0c1c107771a76dd6e73b4313b32323
Slava <emashovvn2@sibmail.com>
2021-04-21 19:42:22 +0700

    Переход с Ольгиного бука

--- COM_tests.py
+++ COM_tests.py
@@ -5,33 +5,28 @@ from time import sleep as sleep
 from shutil import copyfile as cf
 import win32serviceutil
 
-ics = win32com.client.Dispatch("InfinitySecurity.Security")
-
 
 def sec_file_change(source):
-    cf(source, r'C:\Program Files (x86)\EleSy\SCADA Infinity\InfinityClientSecurity\security.isam')
     win32serviceutil.StopService('InfinitySecurityServer')
+    sleep(1)
+    cf(source, r'C:\Program Files (x86)\EleSy\SCADA Infinity\InfinityClientSecurity\security.isam')
     sleep(5)
     win32serviceutil.StartService('InfinitySecurityServer')
-    sleep(15)
-    global ics
-    ics = win32com.client.Dispatch("InfinitySecurity.Security")
-    sleep(2)
+    sleep(25)
 
+@pytest.fixture
+def get_ics_connect():
+    return win32com.client.Dispatch("InfinitySecurity.Security")
 
+    
 @pytest.fixture
 def resetDB_unregUser():
     sec_file_change(r'ICS\origin.isam')
-    #if (ics.GetUserName() != "test_1"):
-    #    ics.UserLogon("test_1", "123")
-    #if (ics.GetUserName() != "test_1"):
-    #    ics.UserLogoff()
-    #ics.SetOriginalDB()
-    ics.UserLogoff()
     
 @pytest.fixture
 def clear_infinty_audit():
-    EventLogWorker.clear_log(logtype = "InfinityAudit", server = 'localhost')
+    pass
+    #EventLogWorker.clear_log(logtype = "InfinityAudit", server = 'localhost')
 
 @pytest.mark.skip(reason='Разобавться с вызовом ISecurityAdit')
 def test_audit_func(clear_infinty_audit):
@@ -46,137 +41,173 @@ def test_qwe():
     sec_file_change(r'ICS\test_1_2_3_block_test.isam')
     pass
 
-def test_load_default_DB(clear_infinty_audit, resetDB_unregUser):
-    assert ics.SetOriginalDB() == True
+def test_load_default_DB(resetDB_unregUser, clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.SetOriginalDB() == True
     assert EventLogWorker.find_event_by_ID("ics_1050") == True
     
-
-
-def test_save_DB(clear_infinty_audit):
-    assert ics.SaveConfig() == True
+def test_get_user_name_unreg(get_ics_connect):
+    assert get_ics_connect.GetUserName() == 'Незарегистрированный'
+    
+def test_save_DB(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.SaveConfig() == True
     assert EventLogWorker.find_event_by_ID("ics_1160_1") == True
     
-def test_role_0_success(clear_infinty_audit):
-    assert ics.Role() == 0
+def test_role_0_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.Role() == 0
 
-def test_empty_GetUserList_success(clear_infinty_audit):
-    assert ics.GetUserList() == (1,("Незарегистрированный",))
+def test_empty_GetUserList_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.GetUserList() == (1,("Незарегистрированный",))
 
-def test_add_user_test100_failure(clear_infinty_audit):
-    assert ics.AddUser("test_100","") == False
+def test_add_user_test100_failure(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.AddUser("test_100","") == False
     assert EventLogWorker.find_event_by_ID("ics_1114") == True
 
-def test_delete_user_test100_failed(clear_infinty_audit):
-    assert ics.DeleteUser("test_100") == False
+def test_delete_user_test100_failed(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.DeleteUser("test_100") == False
     assert EventLogWorker.find_event_by_ID("ics_1118") == True
 
-def test_delete_user_test100_to_group_failed(clear_infinty_audit):
-    assert ics.AddUserGroup("test_100","Администраторы") == False
+def test_delete_user_test100_to_group_failed(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.AddUserGroup("test_100","Администраторы") == False
     assert EventLogWorker.find_event_by_ID("ics_1100") == True
 
-def test_add_user_test_1_success_1(clear_infinty_audit):
-    assert ics.AddUser("test_1","") == True
+def test_add_user_test_1_success_1(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.AddUser("test_1","") == True
     assert EventLogWorker.find_event_by_ID("ics_1027_2") == True
     
-def test_add_user_test_1_failed(clear_infinty_audit):
-    assert ics.AddUser("test_1","") == False
+def test_add_user_test_1_failed(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.AddUser("test_1","") == False
     assert EventLogWorker.find_event_by_ID("ics_1113") == True
 
-def test_add_user_test_1_to_group_failed(clear_infinty_audit):
-    assert ics.AddUserGroup("test_1","Админы") == False
+def test_add_user_test_1_to_group_failed(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.AddUserGroup("test_1","Админы") == False
     assert EventLogWorker.find_event_by_ID("ics_1101") == True
 
-def test_add_user_test_1_to_admins_success(clear_infinty_audit):
-    assert ics.AddUserGroup("test_1","Администраторы") == True
+def test_add_user_test_1_to_admins_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.AddUserGroup("test_1","Администраторы") == True
     assert EventLogWorker.find_event_by_ID("ics_1018_1") == True
 
-def test_add_user_test_1_to_admins_failure(clear_infinty_audit):
-    assert ics.AddUserGroup("test_1","Администраторы") == False
+def test_add_user_test_1_to_admins_failure(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.AddUserGroup("test_1","Администраторы") == False
     assert EventLogWorker.find_event_by_ID("ics_1102") == True
 
-def test_delete_user_test_100_from_group_failed(clear_infinty_audit):
-    assert ics.DeleteUserGroup("test_100","Администраторы") == False
+def test_delete_user_test_100_from_group_failed(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.DeleteUserGroup("test_100","Администраторы") == False
     assert EventLogWorker.find_event_by_ID("ics_1104") == True
 
-def test_delete_user_test_1_from_group_failed(clear_infinty_audit):
-    assert ics.DeleteUserGroup("test_1","Админы") == False
+def test_delete_user_test_1_from_group_failed(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.DeleteUserGroup("test_1","Админы") == False
     assert EventLogWorker.find_event_by_ID("ics_1105") == True
 
-def test_empty_GetUserList2_success(clear_infinty_audit):
-    ics.SaveConfig()
-    assert ics.GetUserList() == (1,("test_1",))
+def test_empty_GetUserList2_success(clear_infinty_audit, get_ics_connect):
+    get_ics_connect.SaveConfig()
+    assert get_ics_connect.GetUserList() == (1,("test_1",))
 
-def test_delete_user_test_1_from_group_success(clear_infinty_audit):
-    assert ics.DeleteUserGroup("test_1","Администраторы") == False
+def test_delete_user_test_1_from_group_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.DeleteUserGroup("test_1","Администраторы") == False
     assert EventLogWorker.find_event_by_ID("ics_1103") == True
-    #ics.SaveConfig()
 
-def test_add_user_test_1_success_2(clear_infinty_audit, resetDB_unregUser):
-    #sleep(5)
-    assert ics.AddUser("test_1","") == True
+def test_add_user_test_1_success_2(clear_infinty_audit, resetDB_unregUser, get_ics_connect):
+    assert get_ics_connect.AddUser("test_1","") == True
     assert EventLogWorker.find_event_by_ID("ics_1027_2") == True
-    #sleep(5)
-    assert ics.AddUserGroup("test_1","Администраторы") == True
+    assert get_ics_connect.AddUserGroup("test_1","Администраторы") == True
     assert EventLogWorker.find_event_by_ID("ics_1018_1") == True
-    #ics.SaveConfig()
-    
     
 #@pytest.mark.skip    
-def test_add_user_test_2_success(clear_infinty_audit):
-    assert ics.AddUser("test_2","") == True
+def test_add_user_test_2_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.AddUser("test_2","") == True
     assert EventLogWorker.find_event_by_ID("ics_1027_3") == True
-    #assert ics.GetUserList() == (2,("test_1","test_2",))
     
 #@pytest.mark.skip
-def test_add_user_test_3_success(clear_infinty_audit):
-    assert ics.AddUser("test_3","") == True
+def test_add_user_test_3_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.AddUser("test_3","") == True
     assert EventLogWorker.find_event_by_ID("ics_1027_4") == True
-    ics.SaveConfig()
-    assert ics.GetUserList() == (3,("test_1","test_2","test_3",))
-    ics.UserLogon("test_1","123")
+    get_ics_connect.SaveConfig()
+    assert get_ics_connect.GetUserList() == (3,("test_1","test_2","test_3",))
+    assert get_ics_connect.UserLogon("test_1","123") == True
+    #assert EventLogWorker.find_event_by_ID("ics_1160_2") == True
     
-def test_delete_user_test_3_success(clear_infinty_audit):
-    #ics.UserLogon("test_1","123")
-    assert ics.DeleteUser("test_3") == True
+def test_get_user_name_test_1(get_ics_connect):
+    assert get_ics_connect.GetUserName() == 'test_1'
+
+def test_login_current_user_success(clear_infinty_audit, get_ics_connect):
+    try:
+        get_ics_connect.UserLogon("test_1","123")
+    except:
+        pass
+    assert EventLogWorker.find_event_by_ID("ics_1061") == True 
+    
+def test_delete_user_test_3_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.DeleteUser("test_3") == True
     assert EventLogWorker.find_event_by_ID("ics_1028") == True
-    ics.SaveConfig()
-    assert ics.GetUserList() == (2,("test_1","test_2",))
+    get_ics_connect.SaveConfig()
+    assert get_ics_connect.GetUserList() == (2,("test_1","test_2",))
+        
+def test_check_user_test_1_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.CheckUser("test_1","123") == True
+
+def test_check_user_test_1_failure(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.CheckUser("test_1","1234") == False
+
+def test_check_user_test_3_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.CheckUser("test_3","123") == True
+
+def test_check_user_test_3_failure(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.CheckUser("test_3","1234") == False
     
-def test_check_user_test_1_success(clear_infinty_audit):
-    assert ics.CheckUser("test_1","123") == True
+def test_get_user_rights_test_1_trends_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.GetUserRights("TREND_START","") == True
 
-def test_check_user_test_1_failure(clear_infinty_audit):
-    assert ics.CheckUser("test_1","1234") == False
-
-def test_check_user_test_3_success(clear_infinty_audit):
-    assert ics.CheckUser("test_3","123") == True
-
-def test_check_user_test_3_failure(clear_infinty_audit):
-    assert ics.CheckUser("test_3","1234") == False
+def test_get_user_rights_test_1_trends_failure(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.GetUserRights("TREND_START_qwe","") == False
     
-def test_get_user_rights_test_1_trends_success(clear_infinty_audit):
-    #ics.UserLogon("test_1","123")
-    assert ics.GetUserRights("TREND_START","") == True
+def test_get_user_rights_test_1_trends_with_condition_success(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.GetUserRights("TREND_TAG","*") == True
 
-def test_get_user_rights_test_1_trends_failure(clear_infinty_audit):
-    #ics.UserLogon("test_1","123")
-    assert ics.GetUserRights("TREND_START_qwe","") == False
+def test_get_user_rights_test_1_trends_with_condition_failure(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.GetUserRights("TREND_TAG","qwe") == True
     
-def test_get_user_rights_test_1_trends_with_condition_success(clear_infinty_audit):
-    #ics.UserLogon("test_1","123")
-    assert ics.GetUserRights("TREND_TAG","*") == True
+def test_get_user_rights_test_2_trends_with_condition_failure(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.GetUserRights("TREND_TAG","") == True
 
-def test_get_user_rights_test_1_trends_with_condition_failure(clear_infinty_audit):
-    #ics.UserLogon("test_1","123")
-    assert ics.GetUserRights("TREND_TAG","qwe") == False
+def test_wrong_password_success(clear_infinty_audit, get_ics_connect):
+    try:
+        get_ics_connect.UserLogon("test_2","1234")
+    except:
+        pass
+    assert EventLogWorker.find_event_by_ID("ics_1022") == True    
     
-
-def test_get_user_rights_test_2_trends_with_condition_failure(clear_infinty_audit):
-    #ics.UserLogon("test_1","123")
-    assert ics.GetUserRights("TREND_TAG","") == False
-    
+def test_wrong_user_failure(clear_infinty_audit, get_ics_connect):
+    try:
+        get_ics_connect.UserLogon("test_222","123")
+    except:
+        pass
+    assert EventLogWorker.find_event_by_ID("ics_1022") == True 
+    assert EventLogWorker.find_event_by_ID("ics_1060") == True
     
 @pytest.mark.skip
-def test_delete_all_users(clear_infinty_audit):
-    assert ics.DeleteAllUsers() == True
-    ics.SaveConfig()
+def test_delete_all_users(clear_infinty_audit, get_ics_connect):
+    assert get_ics_connect.DeleteAllUsers() == True
+    get_ics_connect.SaveConfig()
+
+def test_list_blocked_users_success(get_ics_connect):
+    sec_file_change(r'ICS\test_1_2_3_user_1_2_block_test.isam')
+    get_ics_connect.UserLogon("test_1","123")
+    
+def test_list_groups_success(get_ics_connect):
+    assert get_ics_connect.GetGroupList() == (3,("Администраторы","Test","Блокированные",))
+    
+def test_list_users_in_group_Test_success(get_ics_connect):
+    assert get_ics_connect.GetGroupUsers("Test") == (2,("test_2","test_3",))
+    
+def test_list_users_in_group_Blocked_success(get_ics_connect):
+    assert get_ics_connect.GetGroupUsers("Блокированные") == (0,("",))
+   
+def test_list_users_in_group_Administrators_success(get_ics_connect):
+    assert get_ics_connect.GetGroupUsers("Администраторы") == (1,("test_1",))
+    
+def test_list_blocked_users_success_2(get_ics_connect):
+    assert get_ics_connect.GetBlockedUserList() == (2,("user_1","user_2",))
+    
+@pytest.mark.skip    
+def test_qq(get_ics_connect):
+    assert get_ics_connect.GetBlockedUserList() == (1,("test_2",))
